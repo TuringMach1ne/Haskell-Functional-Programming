12cd0a2e0c1cab151c6228f85b7d1820d159deab
Monch Spaceman <monch@localhost.localdomain>
2021-11-30 23:15:30 +0000

    added exercises

--- Haskell-Exercises.hs
+++ Haskell-Exercises.hs
@@ -22,3 +22,41 @@ x `fun2` y    = foldr (:) y x
 fun3  :: [[a]] -> [a]
 fun3 xs = [xss | xss <- fun1 xs]
 
+----------
+
+deneme = do
+  x <- getLine
+  putStrLn x
+  
+
+hitme = do
+    putStrLn ("Ninety-nine what ?")
+    x <- getLine
+    return (99,x)
+    
+    
+pilate = do
+    putStrLn ("What can I get you?")
+    x <- getLine
+    putStrLn ("Uh, we don't have " ++ x)
+    pilate
+    
+    
+countdown n = do
+    print n
+    if n == 0
+    then do
+        putStrLn "liftoff"
+    else do
+    countdown (n-1)
+    
+    
+getList = do
+    putStr "Who's on the list?"
+    x <- getLine
+    if x == "Nobody"
+    then return []
+    else do
+        putStrLn (x ++ " is on the list!")
+        xs <- getList
+        return (x:xs)
